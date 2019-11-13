ba8932968d15159dc41d8c9eadc6ec12e6ac257f
Ferney Mauricio Arenas <neypaisa@hotmail.com>
2019-11-13 01:39:01 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,9 +8,6 @@ end
 
 # code your input_to_index and move method here!
 
-def input_to_index(input)
-  return index = input.to_i - 1
-end
 
 def move(board, index, character)
   board[index] = character
