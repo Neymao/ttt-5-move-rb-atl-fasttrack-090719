5ffb5e5bbce4db670eb97c823e45a5be25445aee
Ferney Mauricio Arenas <neypaisa@hotmail.com>
2019-11-13 01:58:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,10 +8,7 @@ end
 
 # code your input_to_index and move method here!
 
-board = [" ", " ", " "]
-def update_array_at_with(array, index, value)
-  array[index] = value
-end
+
 
 def move(board, index, character)
   board[index] = character
